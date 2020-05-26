aab99cf8ba69d6c12e0a14bf1c095916e7ea3973
maumruiz <mau.mruiz@hotmail.com>
2020-05-26 02:08:52 -0500

    Create multiverse optimization run function

--- ComputingFundamentals/src/mx/tec/metaheuristics/multiverseoptimizer/MultiverseOptimizer.java
+++ ComputingFundamentals/src/mx/tec/metaheuristics/multiverseoptimizer/MultiverseOptimizer.java
@@ -5,6 +5,18 @@
  */
 package mx.tec.metaheuristics.multiverseoptimizer;
 
+import java.util.Random;
+import mx.tec.hermes.frameworks.HHEvaluator;
+import mx.tec.hermes.frameworks.rulebased.RuleBasedHH;
+import mx.tec.hermes.frameworks.rulebased.RuleBasedHHGenerator;
+import mx.tec.hermes.frameworks.rulebased.RuleBasedHHIndividual;
+import mx.tec.hermes.problems.Problem;
+import mx.tec.hermes.problems.ProblemSet;
+import mx.tec.metaheuristics.Evaluator;
+import mx.tec.metaheuristics.Generator;
+import mx.tec.metaheuristics.evolutionary.Selector;
+import mx.tec.metaheuristics.evolutionary.TournamentSelector;
+
 /**
  *
  * @author Mauricio
@@ -19,3 +31,26 @@ public class MultiverseOptimizer {
     public void saluda() {
         System.out.println(this.saludo);
     }
+    
+    public static RuleBasedHH runMultiverseAlgorithm(Problem problem, ProblemSet set, String[] features, String[] heuristics, int populationSize, int maxEvaluations, double crossoverRate, double mutationRate, boolean printMode, long seed) {
+        Random random = new Random(seed);
+        
+        // Set static features and heuristics for all individuals
+        RuleBasedHHIndividual.setFeatures(features);
+        RuleBasedHHIndividual.setHeuristics(heuristics);
+        
+
+
+        Generator generator = new RuleBasedHHGenerator(random.nextLong());
+        Evaluator evaluator = new HHEvaluator(problem, set);
+        Selector selector = new TournamentSelector(3, random.nextLong());
+        
+                 // Change for multiverse
+//        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(evaluator, generator, selector);;
+        
+//        RuleBasedHHIndividual bestUniverse = multiverse.Run(populationSize, maxEvaluations, crossoverRate, mutationRate, type, printMode)
+//        RuleBasedHH hyperHeuristic = bestUniverse.getHyperHeuristic()
+//        return hyperHeuristic;
+        return new RuleBasedHH("");
+    }
+}
